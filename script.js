72c79cb1c91446df8e080009c491e09b8bdde204
June <adasilva.html@gmail.com>
2022-09-18 19:54:25 -0400

    Slider change grid size but draw no longer working
    added a range slider that chooses value from 0-100
    value is displayed above it
    value is also added to gridsize variable
    when slider changed, a buildGrid function deletess old HTML and adds
    new html based on new grid size
    somehow this made the draw function stop working. maybe event listener??

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 // ----- ----- ----- BUILD GRID ----- ----- -----
 
-// grid size variable
-let gridSize = 50;
+function buildGrid(gridSize) {
 
-// create variable for grid container
-const grid = document.querySelector('.grid');
+    // create variable for grid container
+    const grid = document.querySelector('.grid');
+    grid.innerHTML = '';
 
-// for loop to create individual squares
-for (let i = 0; i < gridSize; i++) {
-    let column = document.createElement('div');
-    column.classList.add('column');
-    for (let j = 0; j < gridSize; j++) {
-        let block = document.createElement('div');
-        block.classList.add('block');
+    // for loop to create individual squares
+    for (let i = 0; i < gridSize; i++) {
+        let column = document.createElement('div');
+        column.classList.add('column');
+        
+        for (let j = 0; j < gridSize; j++) {
+            let block = document.createElement('div');
+            block.classList.add('block');
 
-        column.append(block);
-    }
-    
-    grid.append(column); 
+            column.append(block);
+        };
+        grid.append(column); 
+    }; 
 }
+
+
+
+
 
 // ----- ----- ----- ----- ----- ----- ----- ----- 
 const blocks = document.querySelectorAll('.block');
@@ -31,21 +36,12 @@ blocks.forEach((block) => {
     block.addEventListener('mouseover', (event) => {
         if (event.which == 1) {
             draw(event.target);
-            console.log(event.target);
+            console.log(target);
         };
     });
 }); 
 
 
-
-// stop drawing function
-function stopDrawing() {
-    blocks.forEach((block) => {
-        block.addEventListener('mouseup', () => {
-            draw(block);
-        });
-    });
-};
 
 // draw function 
 function draw(block) {
@@ -53,3 +49,25 @@ function draw(block) {
 };
 
 
+// ----- ----- CONTROLS DASHBOARD ----- -----
+
+// range slider and value display
+let sizeDisplay = document.querySelector('#sizeDisplay');
+let slider = document.querySelector('#sizeSlider');
+sizeDisplay.textContent = `${slider.value}x${slider.value}`;
+
+// change size display everytime 
+slider.addEventListener('mouseup', (event) => {
+    if (event.which == 1) {
+        sizeDisplay.textContent = `${slider.value}x${slider.value}`;
+        buildGrid(slider.value);
+    }
+})
+
+// get grid size function
+function getGridSize() {
+    let slider = document.querySelector('#sizeSlider');
+    return slider.value;
+}
+
+buildGrid(slider.value);
